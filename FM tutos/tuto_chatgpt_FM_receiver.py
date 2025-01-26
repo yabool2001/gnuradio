4ea84fed3d6d0c9fd9e4c26b9a28b8e7650f087e
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2025-01-26 23:09:42 +0100

    aaaa

---
+++ FM tutos/tuto_chatgpt_FM_receiver.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: ChatGPT FM receiver
+# Author: yabool2001
+# Copyright: maciej.zemlo@gmail.pl
+# GNU Radio version: 3.10.10.0
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from PyQt5 import QtCore
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import filter
+from gnuradio.filter import firdes
+from gnuradio import gr
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import iio
+import sip
+
+
+
+class tuto_chatgpt_FM_receiver(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "ChatGPT FM receiver", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("ChatGPT FM receiver")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "tuto_chatgpt_FM_receiver")
+
+        try:
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.tx_attenuation = tx_attenuation = 10
+        self.trans_width = trans_width = 50000
+        self.samp_rate = samp_rate = 2048000
+        self.hw_uri = hw_uri = "ip:192.168.2.1"
+        self.f_radio = f_radio = 103000000
+        self.f_cutoff = f_cutoff = 100000
+        self.decimation = decimation = 10
+        self.audio_rate = audio_rate = 48000
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self._tx_attenuation_range = qtgui.Range(0, 60, 1, 10, 200)
+        self._tx_attenuation_win = qtgui.RangeWidget(self._tx_attenuation_range, self.set_tx_attenuation, "Pluto Tx Attenuation", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._tx_attenuation_win)
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
+            1024, #size
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            f_radio, #fc
+            samp_rate, #bw
+            "", #name
+            1,
+            None # parent
+        )
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis((-140), 10)
+        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(True)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
+        self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
+
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.low_pass_filter_0 = filter.fir_filter_ccf(
+            decimation,
+            firdes.low_pass(
+                1,
+                (samp_rate/decimation),
+                f_cutoff,
+                trans_width,
+                window.WIN_HAMMING,
+                6.76))
+        self.iio_pluto_source_0 = iio.fmcomms2_source_fc32(hw_uri if hw_uri else iio.get_pluto_uri(), [True, True], 32768)
+        self.iio_pluto_source_0.set_len_tag_key('packet_len')
+        self.iio_pluto_source_0.set_frequency(f_radio)
+        self.iio_pluto_source_0.set_samplerate(samp_rate)
+        self.iio_pluto_source_0.set_gain_mode(0, 'fast_attack')
+        self.iio_pluto_source_0.set_gain(0, 40)
+        self.iio_pluto_source_0.set_quadrature(True)
+        self.iio_pluto_source_0.set_rfdc(True)
+        self.iio_pluto_source_0.set_bbdc(True)
+        self.iio_pluto_source_0.set_filter_params('Auto', '', 0, 0)
+        self.iio_pluto_sink_0 = iio.fmcomms2_sink_fc32(hw_uri if hw_uri else iio.get_pluto_uri(), [True, True], 32768, False)
+        self.iio_pluto_sink_0.set_len_tag_key('')
+        self.iio_pluto_sink_0.set_bandwidth(20000000)
+        self.iio_pluto_sink_0.set_frequency(f_radio)
+        self.iio_pluto_sink_0.set_samplerate((audio_rate*4))
+        self.iio_pluto_sink_0.set_attenuation(0, tx_attenuation)
+        self.iio_pluto_sink_0.set_filter_params('Auto', '', 0, 0)
+        self.blocks_wavfile_source_0 = blocks.wavfile_source('C:\\Users\\mzeml\\gnuradio\\FM tutos\\15.Variation_14.wav', True)
+        self.audio_sink_0 = audio.sink(audio_rate, '', True)
+        self.analog_wfm_tx_0 = analog.wfm_tx(
+        	audio_rate=audio_rate,
+        	quad_rate=(audio_rate*4),
+        	tau=(75e-6),
+        	max_dev=75e3,
+        	fh=(-1.0),
+        )
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=(samp_rate/decimation),
+        	audio_decimation=4,
+        )
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_wfm_rcv_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.analog_wfm_tx_0, 0), (self.iio_pluto_sink_0, 0))
+        self.connect((self.blocks_wavfile_source_0, 0), (self.analog_wfm_tx_0, 0))
+        self.connect((self.iio_pluto_source_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.iio_pluto_source_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "tuto_chatgpt_FM_receiver")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_tx_attenuation(self):
+        return self.tx_attenuation
+
+    def set_tx_attenuation(self, tx_attenuation):
+        self.tx_attenuation = tx_attenuation
+        self.iio_pluto_sink_0.set_attenuation(0,self.tx_attenuation)
+
+    def get_trans_width(self):
+        return self.trans_width
+
+    def set_trans_width(self, trans_width):
+        self.trans_width = trans_width
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, (self.samp_rate/self.decimation), self.f_cutoff, self.trans_width, window.WIN_HAMMING, 6.76))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.iio_pluto_source_0.set_samplerate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, (self.samp_rate/self.decimation), self.f_cutoff, self.trans_width, window.WIN_HAMMING, 6.76))
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.f_radio, self.samp_rate)
+
+    def get_hw_uri(self):
+        return self.hw_uri
+
+    def set_hw_uri(self, hw_uri):
+        self.hw_uri = hw_uri
+
+    def get_f_radio(self):
+        return self.f_radio
+
+    def set_f_radio(self, f_radio):
+        self.f_radio = f_radio
+        self.iio_pluto_sink_0.set_frequency(self.f_radio)
+        self.iio_pluto_source_0.set_frequency(self.f_radio)
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.f_radio, self.samp_rate)
+
+    def get_f_cutoff(self):
+        return self.f_cutoff
+
+    def set_f_cutoff(self, f_cutoff):
+        self.f_cutoff = f_cutoff
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, (self.samp_rate/self.decimation), self.f_cutoff, self.trans_width, window.WIN_HAMMING, 6.76))
+
+    def get_decimation(self):
+        return self.decimation
+
+    def set_decimation(self, decimation):
+        self.decimation = decimation
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, (self.samp_rate/self.decimation), self.f_cutoff, self.trans_width, window.WIN_HAMMING, 6.76))
+
+    def get_audio_rate(self):
+        return self.audio_rate
+
+    def set_audio_rate(self, audio_rate):
+        self.audio_rate = audio_rate
+        self.iio_pluto_sink_0.set_samplerate((self.audio_rate*4))
+
+
+
+
+def main(top_block_cls=tuto_chatgpt_FM_receiver, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
