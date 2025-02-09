98c228f2732f6139f758bb27ef56f8b47bfdfce5
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2025-02-09 09:19:12 +0100

    aa

--- constellation_test.py
+++ constellation_test.py
@@ -64,6 +64,7 @@ class constellation_test(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.f_c = f_c = 1000
+        self.sps = sps = f_c*16
         self.samp_rate = samp_rate = f_c*4
         self.P = P = 0
         self.A = A = 1
@@ -99,6 +100,56 @@ class constellation_test(gr.top_block, Qt.QWidget):
         self.qtgui_sink_x_0.enable_rf_freq(False)
 
         self.top_layout.addWidget(self._qtgui_sink_x_0_win)
+        self.qtgui_eye_sink_x_0 = qtgui.eye_sink_c(
+            1024, #size
+            samp_rate, #samp_rate
+            1, #number of inputs
+            None
+        )
+        self.qtgui_eye_sink_x_0.set_update_time(0.10)
+        self.qtgui_eye_sink_x_0.set_samp_per_symbol(sps)
+        self.qtgui_eye_sink_x_0.set_y_axis(-1, 1)
+
+        self.qtgui_eye_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_eye_sink_x_0.enable_tags(True)
+        self.qtgui_eye_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_eye_sink_x_0.enable_autoscale(False)
+        self.qtgui_eye_sink_x_0.enable_grid(False)
+        self.qtgui_eye_sink_x_0.enable_axis_labels(True)
+        self.qtgui_eye_sink_x_0.enable_control_panel(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'blue', 'blue', 'blue', 'blue',
+            'blue', 'blue', 'blue', 'blue', 'blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(2):
+            if len(labels[i]) == 0:
+                if (i % 2 == 0):
+                    self.qtgui_eye_sink_x_0.set_line_label(i, "Eye [Re{{Data {0}}}]".format(round(i/2)))
+                else:
+                    self.qtgui_eye_sink_x_0.set_line_label(i, "Eye [Im{{Data {0}}}]".format(round((i-1)/2)))
+            else:
+                self.qtgui_eye_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_eye_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_eye_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_eye_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_eye_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_eye_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_eye_sink_x_0_win = sip.wrapinstance(self.qtgui_eye_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_eye_sink_x_0_win)
         self.qtgui_const_sink_x_0_0 = qtgui.const_sink_c(
             1024, #size
             "Complex signal", #name
@@ -109,7 +160,7 @@ class constellation_test(gr.top_block, Qt.QWidget):
         self.qtgui_const_sink_x_0_0.set_y_axis((-4), 4)
         self.qtgui_const_sink_x_0_0.set_x_axis((-4), 4)
         self.qtgui_const_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
-        self.qtgui_const_sink_x_0_0.enable_autoscale(True)
+        self.qtgui_const_sink_x_0_0.enable_autoscale(False)
         self.qtgui_const_sink_x_0_0.enable_grid(False)
         self.qtgui_const_sink_x_0_0.enable_axis_labels(True)
 
@@ -157,6 +208,7 @@ class constellation_test(gr.top_block, Qt.QWidget):
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_throttle2_0_0, 0))
         self.connect((self.blocks_float_to_complex_0, 0), (self.qtgui_const_sink_x_0_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.qtgui_eye_sink_x_0, 0))
         self.connect((self.blocks_float_to_complex_0, 0), (self.qtgui_sink_x_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.blocks_float_to_complex_0, 0))
         self.connect((self.blocks_throttle2_0_0, 0), (self.blocks_float_to_complex_0, 1))
@@ -176,8 +228,16 @@ class constellation_test(gr.top_block, Qt.QWidget):
     def set_f_c(self, f_c):
         self.f_c = f_c
         self.set_samp_rate(self.f_c*4)
+        self.set_sps(self.f_c*16)
         self.analog_sig_source_x_0.set_frequency(self.f_c)
         self.analog_sig_source_x_0_0.set_frequency(self.f_c)
+
+    def get_sps(self):
+        return self.sps
+
+    def set_sps(self, sps):
+        self.sps = sps
+        self.qtgui_eye_sink_x_0.set_samp_per_symbol(self.sps)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -188,6 +248,7 @@ class constellation_test(gr.top_block, Qt.QWidget):
         self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
         self.blocks_throttle2_0_0.set_sample_rate(self.samp_rate)
+        self.qtgui_eye_sink_x_0.set_samp_rate(self.samp_rate)
         self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
 
     def get_P(self):
