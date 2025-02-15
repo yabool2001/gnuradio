d08b9d0bddaf047b20ecf7885e9bbc196c5a8e1c
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2025-02-15 10:53:42 +0100

    aa

--- udemy_Moazzam_38_QPSK_nice_visual.py
+++ udemy_Moazzam_38_QPSK_nice_visual.py
@@ -5,17 +5,16 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: QPSK nice visualisation test1
+# Title: udemy Moazzam 38 QPSK nice visualisation
 # Author: yabool2001
 # Copyright: mzemlo.pl@gmail.com
-# Description: QPSK test1
+# Description: 38. QPSK Signal Generation in GNU Radio nice visualisation
 # GNU Radio version: 3.10.10.0
 
 from PyQt5 import Qt
 from gnuradio import qtgui
 from gnuradio import analog
 from gnuradio import blocks
-from gnuradio import digital
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
@@ -29,12 +28,12 @@ import sip
 
 
 
-class yabool2001_QPSK_nice_visualisation_test1(gr.top_block, Qt.QWidget):
+class udemy_Moazzam_38_QPSK_nice_visual(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "QPSK nice visualisation test1", catch_exceptions=True)
+        gr.top_block.__init__(self, "udemy Moazzam 38 QPSK nice visualisation", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("QPSK nice visualisation test1")
+        self.setWindowTitle("udemy Moazzam 38 QPSK nice visualisation")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -52,7 +51,7 @@ class yabool2001_QPSK_nice_visualisation_test1(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "yabool2001_QPSK_nice_visualisation_test1")
+        self.settings = Qt.QSettings("GNU Radio", "udemy_Moazzam_38_QPSK_nice_visual")
 
         try:
             geometry = self.settings.value("geometry")
@@ -64,65 +63,12 @@ class yabool2001_QPSK_nice_visualisation_test1(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.variable_constellation_0 = variable_constellation_0 = digital.constellation_qpsk().base()
-        self.variable_constellation_0.set_npwr(1.0)
         self.samp_rate = samp_rate = 32000
 
         ##################################################
         # Blocks
         ##################################################
 
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
-            2048, #size
-            samp_rate, #samp_rate
-            "", #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
-
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
-
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(True)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
-
-
-        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
-            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ['blue', 'red', 'green', 'black', 'cyan',
-            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-        styles = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1]
-
-
-        for i in range(2):
-            if len(labels[i]) == 0:
-                if (i % 2 == 0):
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
-                else:
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
-            else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.qtgui_const_sink_x_0_0_0 = qtgui.const_sink_c(
             1, #size
             "", #name
@@ -164,18 +110,9 @@ class yabool2001_QPSK_nice_visualisation_test1(gr.top_block, Qt.QWidget):
 
         self._qtgui_const_sink_x_0_0_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0_0_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_const_sink_x_0_0_0_win)
-        self.digital_constellation_modulator_0 = digital.generic_mod(
-            constellation=variable_constellation_0,
-            differential=True,
-            samples_per_symbol=2,
-            pre_diff_code=True,
-            excess_bw=0.35,
-            verbose=False,
-            log=False,
-            truncate=False)
-        self.blocks_vector_source_x_0_1 = blocks.vector_source_b([0,1,2,3], True, 1, [])
-        self.blocks_throttle2_0_0 = blocks.throttle( gr.sizeof_char*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_char*1, samp_rate)
+        self.blocks_vector_source_x_0_1 = blocks.vector_source_c([-1 - 1j, 1 - 1j, 1 + 1j, -1 + 1j], True, 1, [])
+        self.blocks_throttle2_0_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_gr_complex*1, 1000)
         self.blocks_add_xx_0_0 = blocks.add_vcc(1)
         self.analog_noise_source_x_0_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 0.05, 0)
 
@@ -185,41 +122,31 @@ class yabool2001_QPSK_nice_visualisation_test1(gr.top_block, Qt.QWidget):
         ##################################################
         self.connect((self.analog_noise_source_x_0_0, 0), (self.blocks_add_xx_0_0, 1))
         self.connect((self.blocks_add_xx_0_0, 0), (self.qtgui_const_sink_x_0_0_0, 1))
-        self.connect((self.blocks_repeat_0, 0), (self.digital_constellation_modulator_0, 0))
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_add_xx_0_0, 0))
+        self.connect((self.blocks_repeat_0, 0), (self.qtgui_const_sink_x_0_0_0, 0))
         self.connect((self.blocks_throttle2_0_0, 0), (self.blocks_repeat_0, 0))
         self.connect((self.blocks_vector_source_x_0_1, 0), (self.blocks_throttle2_0_0, 0))
-        self.connect((self.digital_constellation_modulator_0, 0), (self.blocks_add_xx_0_0, 0))
-        self.connect((self.digital_constellation_modulator_0, 0), (self.qtgui_const_sink_x_0_0_0, 0))
-        self.connect((self.digital_constellation_modulator_0, 0), (self.qtgui_time_sink_x_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "yabool2001_QPSK_nice_visualisation_test1")
+        self.settings = Qt.QSettings("GNU Radio", "udemy_Moazzam_38_QPSK_nice_visual")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
 
         event.accept()
 
-    def get_variable_constellation_0(self):
-        return self.variable_constellation_0
-
-    def set_variable_constellation_0(self, variable_constellation_0):
-        self.variable_constellation_0 = variable_constellation_0
-
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.blocks_repeat_0.set_interpolation(self.samp_rate)
         self.blocks_throttle2_0_0.set_sample_rate(self.samp_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
 
 
 
-def main(top_block_cls=yabool2001_QPSK_nice_visualisation_test1, options=None):
+def main(top_block_cls=udemy_Moazzam_38_QPSK_nice_visual, options=None):
 
     qapp = Qt.QApplication(sys.argv)
 
