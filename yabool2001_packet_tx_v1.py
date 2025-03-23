0fb939d148f34869eba3d5cecfa4be8557e6728e
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2025-03-23 22:10:01 +0100

    aa

--- yabool2001_packet_tx_v1.py
+++ yabool2001_packet_tx_v1.py
@@ -5,12 +5,14 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Simple Bpsk Tx V2
+# Title: packet tx v1
+# Author: yabool2001
+# Copyright: mzemlo.pl@gmail.com
+# Description: packet tx v1
 # GNU Radio version: 3.10.12.0
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from PyQt5 import QtCore
 from gnuradio import blocks
 import pmt
 from gnuradio import blocks, gr
@@ -25,18 +27,18 @@ from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import iio
-import simple_bpsk_tx_v2_epy_block_0 as epy_block_0  # embedded python block
 import sip
 import threading
+import yabool2001_packet_tx_v1_epy_block_0 as epy_block_0  # embedded python block
 
 
 
-class simple_bpsk_tx_v2(gr.top_block, Qt.QWidget):
+class yabool2001_packet_tx_v1(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Simple Bpsk Tx V2", catch_exceptions=True)
+        gr.top_block.__init__(self, "packet tx v1", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Simple Bpsk Tx V2")
+        self.setWindowTitle("packet tx v1")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -54,7 +56,7 @@ class simple_bpsk_tx_v2(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "simple_bpsk_tx_v2")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "yabool2001_packet_tx_v1")
 
         try:
             geometry = self.settings.value("geometry")
@@ -68,36 +70,17 @@ class simple_bpsk_tx_v2(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.sps = sps = 2
-        self.eb = eb = 0.22
-        self.samp_rate = samp_rate = 200e3
-        self.rx_rrc_taps = rx_rrc_taps = firdes.root_raised_cosine(1, sps,1.0, eb, (15*sps))
-        self.pkt_len = pkt_len = 1000
+        self.samp_rate = samp_rate = 65105
         self.my_constellation = my_constellation = digital.constellation_bpsk().base()
         self.my_constellation.set_npwr(1.0)
-        self.gain = gain = 56
-        self.freq = freq = 483e6-300
-        self.amp = amp = 0.7
+        self.freq = freq = 820000000
 
         ##################################################
         # Blocks
         ##################################################
 
-        self._samp_rate_range = qtgui.Range(200e3, 10e6, 200e3, 200e3, 200)
-        self._samp_rate_win = qtgui.RangeWidget(self._samp_rate_range, self.set_samp_rate, "Sample Rate", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_grid_layout.addWidget(self._samp_rate_win, 0, 0, 1, 1)
-        for r in range(0, 1):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
-        self._freq_range = qtgui.Range(50e6, 3e9, 500e3, 483e6-300, 200)
-        self._freq_win = qtgui.RangeWidget(self._freq_range, self.set_freq, "Frequency", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_grid_layout.addWidget(self._freq_win, 0, 1, 1, 1)
-        for r in range(0, 1):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(1, 2):
-            self.top_grid_layout.setColumnStretch(c, 1)
         self.qtgui_time_sink_x_1 = qtgui.time_sink_c(
-            600, #size
+            128, #size
             samp_rate, #samp_rate
             "", #name
             1, #number of inputs
@@ -155,13 +138,6 @@ class simple_bpsk_tx_v2(gr.top_block, Qt.QWidget):
         self.iio_pluto_sink_0.set_samplerate(int(samp_rate))
         self.iio_pluto_sink_0.set_attenuation(0, 10.0)
         self.iio_pluto_sink_0.set_filter_params('Auto', '', 0, 0)
-        self._gain_range = qtgui.Range(0, 83, 1, 56, 200)
-        self._gain_win = qtgui.RangeWidget(self._gain_range, self.set_gain, "Gain", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_grid_layout.addWidget(self._gain_win, 1, 0, 1, 1)
-        for r in range(1, 2):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(0, 1):
-            self.top_grid_layout.setColumnStretch(c, 1)
         self.epy_block_0 = epy_block_0.pmt_to_stream()
         self.digital_constellation_modulator_0 = digital.generic_mod(
             constellation=my_constellation,
@@ -173,15 +149,8 @@ class simple_bpsk_tx_v2(gr.top_block, Qt.QWidget):
             log=False,
             truncate=False)
         self.blocks_unpack_k_bits_bb_0 = blocks.unpack_k_bits_bb(8)
-        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.intern("0101"), 4000)
+        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.intern("0101"), 2000)
         self.blocks_message_debug_0 = blocks.message_debug(True, gr.log_levels.info)
-        self._amp_range = qtgui.Range(0, 0.9, 0.005, 0.7, 200)
-        self._amp_win = qtgui.RangeWidget(self._amp_range, self.set_amp, "Amplitude", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_grid_layout.addWidget(self._amp_win, 1, 1, 1, 1)
-        for r in range(1, 2):
-            self.top_grid_layout.setRowStretch(r, 1)
-        for c in range(1, 2):
-            self.top_grid_layout.setColumnStretch(c, 1)
 
 
         ##################################################
@@ -196,7 +165,7 @@ class simple_bpsk_tx_v2(gr.top_block, Qt.QWidget):
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "simple_bpsk_tx_v2")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "yabool2001_packet_tx_v1")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -208,14 +177,6 @@ class simple_bpsk_tx_v2(gr.top_block, Qt.QWidget):
 
     def set_sps(self, sps):
         self.sps = sps
-        self.set_rx_rrc_taps(firdes.root_raised_cosine(1, self.sps, 1.0, self.eb, (15*self.sps)))
-
-    def get_eb(self):
-        return self.eb
-
-    def set_eb(self, eb):
-        self.eb = eb
-        self.set_rx_rrc_taps(firdes.root_raised_cosine(1, self.sps, 1.0, self.eb, (15*self.sps)))
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -225,29 +186,11 @@ class simple_bpsk_tx_v2(gr.top_block, Qt.QWidget):
         self.iio_pluto_sink_0.set_samplerate(int(self.samp_rate))
         self.qtgui_time_sink_x_1.set_samp_rate(self.samp_rate)
 
-    def get_rx_rrc_taps(self):
-        return self.rx_rrc_taps
-
-    def set_rx_rrc_taps(self, rx_rrc_taps):
-        self.rx_rrc_taps = rx_rrc_taps
-
-    def get_pkt_len(self):
-        return self.pkt_len
-
-    def set_pkt_len(self, pkt_len):
-        self.pkt_len = pkt_len
-
     def get_my_constellation(self):
         return self.my_constellation
 
     def set_my_constellation(self, my_constellation):
         self.my_constellation = my_constellation
-
-    def get_gain(self):
-        return self.gain
-
-    def set_gain(self, gain):
-        self.gain = gain
 
     def get_freq(self):
         return self.freq
@@ -256,16 +199,10 @@ class simple_bpsk_tx_v2(gr.top_block, Qt.QWidget):
         self.freq = freq
         self.iio_pluto_sink_0.set_frequency(int(self.freq))
 
-    def get_amp(self):
-        return self.amp
-
-    def set_amp(self, amp):
-        self.amp = amp
 
 
 
-
-def main(top_block_cls=simple_bpsk_tx_v2, options=None):
+def main(top_block_cls=yabool2001_packet_tx_v1, options=None):
 
     qapp = Qt.QApplication(sys.argv)
 
