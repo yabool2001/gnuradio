e1d848bba0acd8bb14ac61d51b39f64bf34e2a30
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2025-05-15 19:28:55 +0200

    aa

--- sandbox/Protocol Formatter/Protocol_Formatter_sb_01.py
+++ sandbox/Protocol Formatter/Protocol_Formatter_sb_01.py
@@ -26,7 +26,7 @@ from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import gr, pdu
-import Protocol_Formatter_sb_01_epy_block_1_0 as epy_block_1_0  # embedded python block
+import Protocol_Formatter_sb_01_epy_block_1_0_0 as epy_block_1_0_0  # embedded python block
 import Protocol_Formatter_sb_01_epy_block_1_0_0_0 as epy_block_1_0_0_0  # embedded python block
 import Protocol_Formatter_sb_01_epy_block_1_0_1 as epy_block_1_0_1  # embedded python block
 import threading
@@ -71,7 +71,7 @@ class Protocol_Formatter_sb_01(gr.top_block, Qt.QWidget):
         ##################################################
         self.access_key = access_key = '11110000111100001010101010101010'
         self.samp_rate = samp_rate = 32000
-        self.header = header = digital.header_format_default(access_key, 0)
+        self.header = header = digital.header_format_default ( access_key , 0 )
 
         ##################################################
         # Blocks
@@ -81,10 +81,8 @@ class Protocol_Formatter_sb_01(gr.top_block, Qt.QWidget):
         self.pdu_pdu_to_tagged_stream_0 = pdu.pdu_to_tagged_stream(gr.types.byte_t, 'packet_len')
         self.epy_block_1_0_1 = epy_block_1_0_1.byte_logger(samp_rate=samp_rate, filename="03_byte_rx_log.csv")
         self.epy_block_1_0_0_0 = epy_block_1_0_0_0.byte_logger(samp_rate=samp_rate, filename="02_byte_tx_log.csv")
-        self.epy_block_1_0 = epy_block_1_0.byte_logger(samp_rate=samp_rate, filename="04_byte_rx_log.csv")
+        self.epy_block_1_0_0 = epy_block_1_0_0.byte_logger(samp_rate=samp_rate, filename="01_byte_tx_log.csv")
         self.digital_protocol_formatter_bb_0 = digital.protocol_formatter_bb(header, "packet_len")
-        self.digital_crc_check_0 = digital.crc_check(32, 0x4C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, True, True, False, True, 0)
-        self.digital_crc_append_0 = digital.crc_append(32, 0x4C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, True, True, False, 0)
         self.digital_correlate_access_code_xx_ts_1_0 = digital.correlate_access_code_bb_ts(access_key,
           0, "packet_len")
         self.blocks_unpack_k_bits_bb_0 = blocks.unpack_k_bits_bb(8)
@@ -98,19 +96,17 @@ class Protocol_Formatter_sb_01(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.msg_connect((self.blocks_message_strobe_1, 'strobe'), (self.digital_crc_append_0, 'in'))
-        self.msg_connect((self.digital_crc_append_0, 'out'), (self.pdu_pdu_to_tagged_stream_0, 'pdus'))
-        self.msg_connect((self.digital_crc_check_0, 'ok'), (self.blocks_message_debug_0, 'print'))
-        self.msg_connect((self.pdu_tagged_stream_to_pdu_0, 'pdus'), (self.digital_crc_check_0, 'in'))
-        self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.epy_block_1_0, 0))
-        self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.pdu_tagged_stream_to_pdu_0, 0))
+        self.msg_connect((self.blocks_message_strobe_1, 'strobe'), (self.pdu_pdu_to_tagged_stream_0, 'pdus'))
+        self.msg_connect((self.pdu_tagged_stream_to_pdu_0, 'pdus'), (self.blocks_message_debug_0, 'print'))
+        self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.epy_block_1_0_1, 0))
         self.connect((self.blocks_tagged_stream_mux_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.blocks_tagged_stream_mux_0, 0), (self.epy_block_1_0_0_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.blocks_unpack_k_bits_bb_0, 0))
         self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.digital_correlate_access_code_xx_ts_1_0, 0))
         self.connect((self.digital_correlate_access_code_xx_ts_1_0, 0), (self.blocks_pack_k_bits_bb_0, 0))
-        self.connect((self.digital_correlate_access_code_xx_ts_1_0, 0), (self.epy_block_1_0_1, 0))
+        self.connect((self.digital_correlate_access_code_xx_ts_1_0, 0), (self.pdu_tagged_stream_to_pdu_0, 0))
         self.connect((self.digital_protocol_formatter_bb_0, 0), (self.blocks_tagged_stream_mux_0, 0))
+        self.connect((self.digital_protocol_formatter_bb_0, 0), (self.epy_block_1_0_0, 0))
         self.connect((self.pdu_pdu_to_tagged_stream_0, 0), (self.blocks_tagged_stream_mux_0, 1))
         self.connect((self.pdu_pdu_to_tagged_stream_0, 0), (self.digital_protocol_formatter_bb_0, 0))
 
@@ -128,7 +124,7 @@ class Protocol_Formatter_sb_01(gr.top_block, Qt.QWidget):
 
     def set_access_key(self, access_key):
         self.access_key = access_key
-        self.set_header(digital.header_format_default(self.access_key, 0))
+        self.set_header(digital.header_format_default ( self.access_key , 0 ))
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -136,7 +132,7 @@ class Protocol_Formatter_sb_01(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
-        self.epy_block_1_0.samp_rate = self.samp_rate
+        self.epy_block_1_0_0.samp_rate = self.samp_rate
         self.epy_block_1_0_0_0.samp_rate = self.samp_rate
         self.epy_block_1_0_1.samp_rate = self.samp_rate
 
