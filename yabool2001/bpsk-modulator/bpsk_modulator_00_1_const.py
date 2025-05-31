c21788dafa4061304633b7844841ed5b6a2e93db
Maciek Zemlo <38382353+yabool2001@users.noreply.github.com>
2025-05-31 23:50:37 +0200

    aa

--- yabool2001/bpsk-modulator/bpsk_modulator_00_1_const.py
+++ yabool2001/bpsk-modulator/bpsk_modulator_00_1_const.py
@@ -26,7 +26,6 @@ from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import iio
 import bpsk_modulator_00_1_const_epy_block_0_0 as epy_block_0_0  # embedded python block
-import bpsk_modulator_00_1_const_epy_block_0_0_0 as epy_block_0_0_0  # embedded python block
 import sip
 import threading
 
@@ -70,65 +69,22 @@ class bpsk_modulator_00_1_const(gr.top_block, Qt.QWidget):
         ##################################################
         self.sps = sps = 4
         self.samp_rate = samp_rate = 521100
-        self.rx_gain = rx_gain = 0.1
         self.nfilts = nfilts = 32
         self.my_constellation = my_constellation = digital.constellation_bpsk().base()
         self.my_constellation.set_npwr(1.0)
         self.f_c = f_c = 2900000000
         self.ctx_usb = ctx_usb = "usb:"
         self.ctx_ip = ctx_ip = "ip:192.168.2.1"
-        self.bw = bw = 2000000
-        self.att_range = att_range = 1.1
+        self.bw = bw = 1000000
+        self.att_range = att_range = 1.2
 
         ##################################################
         # Blocks
         ##################################################
 
-        self._att_range_range = qtgui.Range(1.0, 70.0, 1, 1.1, 200)
+        self._att_range_range = qtgui.Range(1.0, 70.0, 1, 1.2, 200)
         self._att_range_win = qtgui.RangeWidget(self._att_range_range, self.set_att_range, "Attenuation", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._att_range_win)
-        self.qtgui_freq_sink_x_0_0 = qtgui.freq_sink_c(
-            1024, #size
-            window.WIN_BLACKMAN_hARRIS, #wintype
-            f_c, #fc
-            samp_rate, #bw
-            "Pluto Source", #name
-            1,
-            None # parent
-        )
-        self.qtgui_freq_sink_x_0_0.set_update_time(0.10)
-        self.qtgui_freq_sink_x_0_0.set_y_axis((-140), 10)
-        self.qtgui_freq_sink_x_0_0.set_y_label('Relative Gain', 'dB')
-        self.qtgui_freq_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
-        self.qtgui_freq_sink_x_0_0.enable_autoscale(False)
-        self.qtgui_freq_sink_x_0_0.enable_grid(False)
-        self.qtgui_freq_sink_x_0_0.set_fft_average(1.0)
-        self.qtgui_freq_sink_x_0_0.enable_axis_labels(True)
-        self.qtgui_freq_sink_x_0_0.enable_control_panel(False)
-        self.qtgui_freq_sink_x_0_0.set_fft_window_normalized(False)
-
-
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-            "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_freq_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_freq_sink_x_0_0.set_line_label(i, labels[i])
-            self.qtgui_freq_sink_x_0_0.set_line_width(i, widths[i])
-            self.qtgui_freq_sink_x_0_0.set_line_color(i, colors[i])
-            self.qtgui_freq_sink_x_0_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_freq_sink_x_0_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_0_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
@@ -171,47 +127,6 @@ class bpsk_modulator_00_1_const(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.qtgui_const_sink_x_0_0 = qtgui.const_sink_c(
-            1, #size
-            "Pluto Source", #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_const_sink_x_0_0.set_update_time(0.10)
-        self.qtgui_const_sink_x_0_0.set_y_axis((-2), 2)
-        self.qtgui_const_sink_x_0_0.set_x_axis((-2), 2)
-        self.qtgui_const_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
-        self.qtgui_const_sink_x_0_0.enable_autoscale(False)
-        self.qtgui_const_sink_x_0_0.enable_grid(False)
-        self.qtgui_const_sink_x_0_0.enable_axis_labels(True)
-
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-            "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        styles = [0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0]
-        markers = [0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_const_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_const_sink_x_0_0.set_line_label(i, labels[i])
-            self.qtgui_const_sink_x_0_0.set_line_width(i, widths[i])
-            self.qtgui_const_sink_x_0_0.set_line_color(i, colors[i])
-            self.qtgui_const_sink_x_0_0.set_line_style(i, styles[i])
-            self.qtgui_const_sink_x_0_0.set_line_marker(i, markers[i])
-            self.qtgui_const_sink_x_0_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_const_sink_x_0_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_const_sink_x_0_0_win)
         self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
             1, #size
             "Constellation Modulator", #name
@@ -253,16 +168,6 @@ class bpsk_modulator_00_1_const(gr.top_block, Qt.QWidget):
 
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
-        self.iio_pluto_source_0 = iio.fmcomms2_source_fc32(ctx_usb if ctx_usb else iio.get_pluto_uri(), [True, True], 32768)
-        self.iio_pluto_source_0.set_len_tag_key('packet_len')
-        self.iio_pluto_source_0.set_frequency(f_c)
-        self.iio_pluto_source_0.set_samplerate(samp_rate)
-        self.iio_pluto_source_0.set_gain_mode(0, 'manual')
-        self.iio_pluto_source_0.set_gain(0, rx_gain)
-        self.iio_pluto_source_0.set_quadrature(True)
-        self.iio_pluto_source_0.set_rfdc(True)
-        self.iio_pluto_source_0.set_bbdc(True)
-        self.iio_pluto_source_0.set_filter_params('Auto', '', 0, 0)
         self.iio_pluto_sink_0 = iio.fmcomms2_sink_fc32(ctx_ip if ctx_ip else iio.get_pluto_uri(), [True, True], 32768, False)
         self.iio_pluto_sink_0.set_len_tag_key('')
         self.iio_pluto_sink_0.set_bandwidth(bw)
@@ -270,7 +175,6 @@ class bpsk_modulator_00_1_const(gr.top_block, Qt.QWidget):
         self.iio_pluto_sink_0.set_samplerate(samp_rate)
         self.iio_pluto_sink_0.set_attenuation(0, att_range)
         self.iio_pluto_sink_0.set_filter_params('Auto', '', 0, 0)
-        self.epy_block_0_0_0 = epy_block_0_0_0.complex_sps_file_sink(samp_rate=samp_rate, sps=sps, filename="02_complex_rx_bpsk_mod_log.csv")
         self.epy_block_0_0 = epy_block_0_0.complex_sps_file_sink(samp_rate=samp_rate, sps=sps, filename="01_complex_tx_bpsk_mod_log.csv")
         self.digital_constellation_modulator_0 = digital.generic_mod(
             constellation=my_constellation,
@@ -292,9 +196,6 @@ class bpsk_modulator_00_1_const(gr.top_block, Qt.QWidget):
         self.connect((self.digital_constellation_modulator_0, 0), (self.iio_pluto_sink_0, 0))
         self.connect((self.digital_constellation_modulator_0, 0), (self.qtgui_const_sink_x_0, 0))
         self.connect((self.digital_constellation_modulator_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.iio_pluto_source_0, 0), (self.epy_block_0_0_0, 0))
-        self.connect((self.iio_pluto_source_0, 0), (self.qtgui_const_sink_x_0_0, 0))
-        self.connect((self.iio_pluto_source_0, 0), (self.qtgui_freq_sink_x_0_0, 0))
 
 
     def closeEvent(self, event):
@@ -311,7 +212,6 @@ class bpsk_modulator_00_1_const(gr.top_block, Qt.QWidget):
     def set_sps(self, sps):
         self.sps = sps
         self.epy_block_0_0.sps = self.sps
-        self.epy_block_0_0_0.sps = self.sps
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -319,18 +219,8 @@ class bpsk_modulator_00_1_const(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.epy_block_0_0.samp_rate = self.samp_rate
-        self.epy_block_0_0_0.samp_rate = self.samp_rate
         self.iio_pluto_sink_0.set_samplerate(self.samp_rate)
-        self.iio_pluto_source_0.set_samplerate(self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(self.f_c, self.samp_rate)
-        self.qtgui_freq_sink_x_0_0.set_frequency_range(self.f_c, self.samp_rate)
-
-    def get_rx_gain(self):
-        return self.rx_gain
-
-    def set_rx_gain(self, rx_gain):
-        self.rx_gain = rx_gain
-        self.iio_pluto_source_0.set_gain(0, self.rx_gain)
 
     def get_nfilts(self):
         return self.nfilts
@@ -350,9 +240,7 @@ class bpsk_modulator_00_1_const(gr.top_block, Qt.QWidget):
     def set_f_c(self, f_c):
         self.f_c = f_c
         self.iio_pluto_sink_0.set_frequency(self.f_c)
-        self.iio_pluto_source_0.set_frequency(self.f_c)
         self.qtgui_freq_sink_x_0.set_frequency_range(self.f_c, self.samp_rate)
-        self.qtgui_freq_sink_x_0_0.set_frequency_range(self.f_c, self.samp_rate)
 
     def get_ctx_usb(self):
         return self.ctx_usb
